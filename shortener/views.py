f7c60da1750389add59ae93a7291219ef5c1933e
ubuntuarif <arif.cse28@gmail.com>
2017-05-13 15:20:30 +0600

    Added homeView and template.

--- shortener/views.py
+++ shortener/views.py
@@ -6,12 +6,14 @@ from .models import KirrURL
 
 # Create your views here.
 
+# def kirr_redirect_view(request, shortcode=None, *args, **kwargs):  # function based view FBV
+#     obj = get_object_or_404(KirrURL, shortcode=shortcode)
+#     print(obj.url)
+#     return HttpResponseRedirect(obj.url)
 
-def kirr_redirect_view(request, shortcode=None, *args, **kwargs):  # function based view FBV
-    obj = get_object_or_404(KirrURL, shortcode=shortcode)
-    print(obj.url)
-    return HttpResponseRedirect(obj.url)
-
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'shortener/home.html', {})
 
 class KirrCBView(View):  # class based view
     def get(self, request, shortcode=None, *args, **kwargs):
